161f5ae60c060c547df9a2ebff62746579f228c6
Nidhirani91 <65483963+Nidhirani91@users.noreply.github.com>
2021-09-20 13:31:53 +0100

    Update Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -1,9 +1,8 @@
 def count_dict(arr):
-	count={}
-	for i in arr:
-		if i not in count:
-			count[i]=0
-		count[i]+=1
-	return count
+    count={}
+    for i in arr:
+        count[i] = count.get(i, 0) + 1
+    return count
+
 arr=[1,2,8,9,12,46,76,82,15,20,30]
 print(count_dict(arr))
